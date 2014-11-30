4b973b2a3cb615a191f9be1ede72823c190e4ab2
Chris Bentley <chris@MacBook-Pro.home>
2014-11-30 13:59:33 +0000

    initial script setup for arguments

--- navigation.py
+++ navigation.py
@@ -2,8 +2,18 @@
 
 #  Program to find the x,y coordinates of a spaceship
 
-
 import argparse
+import re
+
+parser = argparse.ArgumentParser(description='This is a script to navigate a probe to th location of a spaceship')
+parser.add_argument('-e','--email', help='An email address to make GET requests with', required=True)
+args = parser.parse_args()
 
 
-print '\nhello world!\n'
+def main(email=args.email):
+    print email
+    print '\nend of script\n'
+
+if __name__ == "__main__":
+    main()
+
